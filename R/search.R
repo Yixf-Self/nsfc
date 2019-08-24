24a8f044dc146d283dd7b3e843d4a83aedf6670f
zjcn86 <33260177+yikeshu0611@users.noreply.github.com>
2019-08-24 15:22:22 +0800

    add search

--- R/search.R
+++ R/search.R
@@ -13,7 +13,7 @@
 #' @export
 #'
 #' @examples abstract(yearStart=2018)
-abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fundStart,fundEnd){
+search <- function(url,header,search,subject,yearStart,yearEnd,itemCategory,fundStart,fundEnd){
     inner_Add_Symbol <- function(character,symbol="+"){
         if (length(character)>=2){
             for (character.i in 1:length(character)) {
@@ -27,6 +27,14 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
             adj=character
         }
         adj
+    }
+    "%s=%" <- function(a,b){
+        loc=list()
+        for (i in 1:length(a)) {
+            loc=c(loc,list(grep(a[i],b)))
+            names(loc)[i]=a[i]
+        }
+        loc
     }
     library(httr)
     library(rvest)
@@ -70,8 +78,9 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                 if (missing(header)) r <- GET(url)
                 if (!missing(header)) r <- GET(url,add_headers(.headers = header))
             }
-        }else if (status_code(r) != 200)
+        }else if (status_code(r) != 200){
             stop(status_code(r),tmcn::toUTF8(':\u67E5\u8BE2\u7F51\u9875\u51FA\u9519,\u8BF7\u91CD\u65B0\u8D4B\u503C\u5173\u952E\u5B57,\u6216\u7A0D\u540E\u518D\u6B21\u8FDB\u884C,\u6216\u8BBE\u7F6Eheader\u540E\u518D\u8FDB\u884C'))
+        }
         r_content=content(r)
         all_items = r_content %>%
             html_nodes(xpath = '//*[@id="l"]/b[1]') %>%
@@ -79,65 +88,16 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
             as.numeric()
         page_number = ceiling(all_items/10)
         url=paste0(url,"&page=",1:page_number)
-        cat(tmcn::toUTF8('\u5171\u6709'),all_items,
-            tmcn::toUTF8('\u4E2A\u9879\u76EE'),'\n')
+        cat(tmcn::toUTF8('\u5BF9'),all_items,
+            tmcn::toUTF8('\u4E2A\u9879\u76EE\u8FDB\u884C\u7B5B\u9009'),'\n')
         cat(tmcn::toUTF8('\u5171\u6709'),page_number,
             tmcn::toUTF8('\u4E2A\u7F51\u9875'),'\n')
         #url is not missing
-    }else if (any(grepl('search',url))){ #theme page url
-        #get total page number
-        if (missing(header)) r <- GET(url)
-        if (!missing(header)) r <- GET(url,add_headers(.headers = header))
-        if (status_code(r)==429){
-            head_response=headers(r)
-            sleep_time=head_response$`retry-after`
-            cat(tmcn::toUTF8('\u8BF7\u7B49\u5F85'),as.numeric(sleep_time)+20,tmcn::toUTF8('\u79D2'),'\n')
-            a = Sys.time()
-            cat(tmcn::toUTF8('\u73B0\u5728\u65F6\u95F4:'),as.character(a),'\n')
-            cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
-                as.character(a+as.numeric(sleep_time)+20),'\n')
-            Sys.sleep(as.numeric(sleep_time)+20)
+    }else if(!missing(url)){
+        if (any(grepl('search',url))){ #theme page url
+            #get total page number
             if (missing(header)) r <- GET(url)
             if (!missing(header)) r <- GET(url,add_headers(.headers = header))
-            if (status_code(r)==429){
-                head_response=headers(r)
-                sleep_time=head_response$`retry-after`
-                cat(tmcn::toUTF8('\u8BF7\u7B49\u5F85'),as.numeric(sleep_time)+40,tmcn::toUTF8('\u79D2'),'\n')
-                a = Sys.time()
-                cat(tmcn::toUTF8('\u73B0\u5728\u65F6\u95F4:'),as.character(a),'\n')
-                cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
-                    as.character(a+as.numeric(sleep_time)+20),'\n')
-                Sys.sleep(as.numeric(sleep_time)+40)
-                if (missing(header)) r <- GET(url)
-                if (!missing(header)) r <- GET(url,add_headers(.headers = header))
-            }
-        }else if (status_code(r) != 200)
-            stop(status_code(r),tmcn::toUTF8(':\u67E5\u8BE2\u7F51\u9875\u51FA\u9519,\u8BF7\u91CD\u65B0\u8D4B\u503C\u5173\u952E\u5B57,\u6216\u7A0D\u540E\u518D\u6B21\u8FDB\u884C,\u6216\u8BBE\u7F6Eheader\u540E\u518D\u8FDB\u884C'))
-        r_content=content(r)
-        all_items = r_content %>%
-            html_nodes(xpath = '//*[@id="l"]/b[1]') %>%
-            html_text(trim = TRUE) %>%
-            as.numeric()
-        page_number = length(url)
-    }else if(any(grepl('project',url))){ #abstract url
-        page_number=length(url)
-    }else{ #search and project represent 2 types of url
-        stop(tmcn::toUTF8('\u8BF7\u68C0\u67E5\u7F51\u5740\u662F\u5426\u6B63\u786E'))
-    }
-    #if page_number is not zero, we will conduct
-    if (page_number != 0){
-        # scrab
-        cat(tmcn::toUTF8('\u9875\u7801:\u9879\u76EE\u5E8F\u53F7\n'))
-        for (i in 1:page_number) {
-            if (i==1) {
-                df=data.frame()
-                df_theme=data.frame()
-                df_theme_nokey=data.frame()
-            }
-            # scrab theme part
-            if (missing(header)) r <- GET(url[i])
-            if (!missing(header)) r <- GET(url[i],add_headers(.headers = header))
-            #check request successfully
             if (status_code(r)==429){
                 head_response=headers(r)
                 sleep_time=head_response$`retry-after`
@@ -147,8 +107,8 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                 cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
                     as.character(a+as.numeric(sleep_time)+20),'\n')
                 Sys.sleep(as.numeric(sleep_time)+20)
-                if (missing(header)) r <- GET(url[i])
-                if (!missing(header)) r <- GET(url[i],add_headers(.headers =header))
+                if (missing(header)) r <- GET(url)
+                if (!missing(header)) r <- GET(url,add_headers(.headers = header))
                 if (status_code(r)==429){
                     head_response=headers(r)
                     sleep_time=head_response$`retry-after`
@@ -158,22 +118,75 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                     cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
                         as.character(a+as.numeric(sleep_time)+20),'\n')
                     Sys.sleep(as.numeric(sleep_time)+40)
+                    if (missing(header)) r <- GET(url)
+                    if (!missing(header)) r <- GET(url,add_headers(.headers = header))
+                }
+            }else if (status_code(r) != 200)
+                stop(status_code(r),tmcn::toUTF8(':\u67E5\u8BE2\u7F51\u9875\u51FA\u9519,\u8BF7\u91CD\u65B0\u8D4B\u503C\u5173\u952E\u5B57,\u6216\u7A0D\u540E\u518D\u6B21\u8FDB\u884C,\u6216\u8BBE\u7F6Eheader\u540E\u518D\u8FDB\u884C'))
+            r_content=content(r)
+            all_items = r_content %>%
+                html_nodes(xpath = '//*[@id="l"]/b[1]') %>%
+                html_text(trim = TRUE) %>%
+                as.numeric()
+            page_number = length(url)
+        }
+    }
+    #if page_number is not zero, we will conduct
+    if (page_number != 0){
+        # scrab
+        for (i in 1:page_number) {
+            cat(paste0(tmcn::toUTF8('\u7B2C'),i,tmcn::toUTF8('\u9875')))
+            if (i==1) {
+                df=data.frame()
+                df_theme=data.frame()
+                df_theme_nokey=data.frame()
+            }
+            # scrab theme part and check
+            {
+                if (missing(header)) r <- GET(url[i])
+                if (!missing(header)) r <- GET(url[i],add_headers(.headers = header))
+                #check request successfully
+                if (status_code(r)==429){
+                    head_response=headers(r)
+                    sleep_time=head_response$`retry-after`
+                    cat(tmcn::toUTF8('\u8BF7\u7B49\u5F85'),as.numeric(sleep_time)+20,tmcn::toUTF8('\u79D2'),'\n')
+                    a = Sys.time()
+                    cat(tmcn::toUTF8('\u73B0\u5728\u65F6\u95F4:'),as.character(a),'\n')
+                    cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
+                        as.character(a+as.numeric(sleep_time)+20),'\n')
+                    Sys.sleep(as.numeric(sleep_time)+20)
                     if (missing(header)) r <- GET(url[i])
                     if (!missing(header)) r <- GET(url[i],add_headers(.headers =header))
+                    if (status_code(r)==429){
+                        head_response=headers(r)
+                        sleep_time=head_response$`retry-after`
+                        cat(tmcn::toUTF8('\u8BF7\u7B49\u5F85'),as.numeric(sleep_time)+40,tmcn::toUTF8('\u79D2'),'\n')
+                        a = Sys.time()
+                        cat(tmcn::toUTF8('\u73B0\u5728\u65F6\u95F4:'),as.character(a),'\n')
+                        cat(tmcn::toUTF8('\u518D\u6B21\u5F00\u59CB\u65F6\u95F4:'),
+                            as.character(a+as.numeric(sleep_time)+20),'\n')
+                        Sys.sleep(as.numeric(sleep_time)+40)
+                        if (missing(header)) r <- GET(url[i])
+                        if (!missing(header)) r <- GET(url[i],add_headers(.headers =header))
+                    }
+                }else if (status_code(r) != 200) {
+                    return(df)
+                    stop(status_code(r),
+                         tmcn::toUTF8(':\u83B7\u53D6\u7F51\u9875\u51FA\u9519,\u8BF7\u7A0D\u540E\u518D\u5C1D\u8BD5'))
                 }
-            }else if (status_code(r) != 200) {
-                return(df)
-                stop(status_code(r),
-                     tmcn::toUTF8(':\u83B7\u53D6\u7F51\u9875\u51FA\u9519,\u8BF7\u7A0D\u540E\u518D\u5C1D\u8BD5'))
             }
             r_content=content(r)
-            # theme page url
-            if (grepl('search',url[i])){
-                all_items = r_content %>%
-                    html_nodes(xpath = '//*[@id="l"]/b[1]') %>%
-                    html_text(trim = TRUE) %>%
-                    as.numeric()
-                if (all_items==0) next(i) # no items in this page
+            # all_items
+            all_items = r_content %>%
+                html_nodes(xpath = '//*[@id="l"]/b[1]') %>%
+                html_text(trim = TRUE) %>%
+                as.numeric()
+            if (all_items==0) {
+                cat('\n')
+                next(i) # no items in this page
+            }
+            #get 9 objects
+            {
                 #1. study_type
                 study_type = r_content %>%
                     html_nodes(xpath = '//*[@id="resultLst"]//div/p[1]/i') %>%
@@ -220,47 +233,47 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                     html_nodes(xpath = '//*[@id="resultLst"]//div/p[2]/span[2]/i') %>%
                     html_text(trim = TRUE) %>%
                     gsub(pattern = " ",replacement = "")
-                ###data.frame.i
-                # df_theme
-                if (!missing(subject)) {
-                    df_theme = data.frame(subject,study_type,item,person,department,
-                                          id,year,fund,abstract_url)
-                    colnames(df_theme)=c(tmcn::toUTF8('\u5B66\u79D1\u5206\u7C7B'),
-                                         tmcn::toUTF8('\u7814\u7A76\u7C7B\u578B'),
-                                         tmcn::toUTF8('\u9879\u76EE\u540D\u79F0'),
-                                         tmcn::toUTF8('\u8D1F\u8D23\u4EBA'),
-                                         tmcn::toUTF8('\u5355\u4F4D'),
-                                         tmcn::toUTF8('\u9879\u76EE\u7F16\u53F7'),
-                                         tmcn::toUTF8('\u6279\u51C6\u5E74\u5EA6'),
-                                         tmcn::toUTF8('\u91D1\u989D'),
-                                         tmcn::toUTF8('\u6458\u8981\u94FE\u63A5'))
-                }else{
-                    df_theme = data.frame(study_type,item,person,department,
-                                          id,year,fund,abstract_url)
-                    colnames(df_theme)=c(tmcn::toUTF8('\u7814\u7A76\u7C7B\u578B'),
-                                         tmcn::toUTF8('\u9879\u76EE\u540D\u79F0'),
-                                         tmcn::toUTF8('\u8D1F\u8D23\u4EBA'),
-                                         tmcn::toUTF8('\u5355\u4F4D'),
-                                         tmcn::toUTF8('\u9879\u76EE\u7F16\u53F7'),
-                                         tmcn::toUTF8('\u6279\u51C6\u5E74\u5EA6'),
-                                         tmcn::toUTF8('\u91D1\u989D'),
-                                         tmcn::toUTF8('\u6458\u8981\u94FE\u63A5'))
-                }
-                #divide df_them to two parts: has abstract or not
-                if (length(abstract_url[nchar(Key)==0])>0){ #no abastrct
-                    cat(tmcn::toUTF8('\u4EE5\u4E0B'),length(abstract_url[nchar(Key)==0]),
-                        tmcn::toUTF8('\u4E2A\u6458\u8981\u94FE\u63A5\u65E0\u6458\u8981'),'\n')
-                    cat(inner_Add_Symbol(abstract_url[nchar(Key)==0],'\n'),'\n')
-                }
-                df_theme_nokey.i=df_theme[nchar(Key)==0,]
-                df_theme_nokey=rbind(df_theme_nokey,df_theme_nokey.i)
-                df_theme=df_theme[nchar(Key)!=0,]
-                abstract_url=abstract_url[nchar(Key)!=0]
-            }else if (grepl('project',url[i])) {
-                abstract_url=url[i]
             }
-            # scrab abstract part by abstract_url
-            if (length(abstract_url)>0){
+            ###data.frame.i
+            # df_theme
+            if (!missing(subject)) {
+                df_theme = data.frame(subject,study_type,item,person,department,
+                                      id,year,fund,abstract_url)
+                colnames(df_theme)=c(tmcn::toUTF8('\u5B66\u79D1\u5206\u7C7B'),
+                                     tmcn::toUTF8('\u7814\u7A76\u7C7B\u578B'),
+                                     tmcn::toUTF8('\u9879\u76EE\u540D\u79F0'),
+                                     tmcn::toUTF8('\u8D1F\u8D23\u4EBA'),
+                                     tmcn::toUTF8('\u5355\u4F4D'),
+                                     tmcn::toUTF8('\u9879\u76EE\u7F16\u53F7'),
+                                     tmcn::toUTF8('\u6279\u51C6\u5E74\u5EA6'),
+                                     tmcn::toUTF8('\u91D1\u989D'),
+                                     tmcn::toUTF8('\u6458\u8981\u94FE\u63A5'))
+            }else{
+                df_theme = data.frame(study_type,item,person,
+                                      department,
+                                      id,year,fund,abstract_url)
+                colnames(df_theme)=c(tmcn::toUTF8('\u7814\u7A76\u7C7B\u578B'),
+                                     tmcn::toUTF8('\u9879\u76EE\u540D\u79F0'),
+                                     tmcn::toUTF8('\u8D1F\u8D23\u4EBA'),
+                                     tmcn::toUTF8('\u5355\u4F4D'),
+                                     tmcn::toUTF8('\u9879\u76EE\u7F16\u53F7'),
+                                     tmcn::toUTF8('\u6279\u51C6\u5E74\u5EA6'),
+                                     tmcn::toUTF8('\u91D1\u989D'),
+                                     tmcn::toUTF8('\u6458\u8981\u94FE\u63A5'))
+            }
+            #select search part
+            search_item=unlist(search %s=% item)[duplicated(unlist(search %s=% item))]
+            if (length(search_item)==0) {
+                cat('\n')
+                next(i) # no items in this page
+            }
+            #if length(search_item) >= 1
+            df_theme = df_theme[search_item,]
+            abstract_url=abstract_url[search_item]
+            Key = Key[search_item]
+            #use key to extract abstract
+            if (any((nchar(Key)>0))){
+                # scrab abstract part by abstract_url
                 for (j in 1:length(abstract_url)) {
                     if (j==1) df_abstract = data.frame()
                     abs_url.j=abstract_url[j]
@@ -339,7 +352,7 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                     df_abstract.j=data.frame(subject_abs,time_limit,
                                              key_word_cn,key_word_english,
                                              abs_cn,abs_english)
-                    df_abstract=rbind(df_abstract,df_abstract.j)
+                    df_abstract=plyr::rbind.fill(df_abstract,df_abstract.j)
                     if (j == length(abstract_url)){
                         #rename
                         colnames(df_abstract)=c(tmcn::toUTF8('\u5B66\u79D1\u5206\u7C7B_\u6765\u81EA\u6458\u8981'),
@@ -350,17 +363,18 @@ abstract <- function(url,header,subject,search,yearStart,yearEnd,itemCategory,fu
                                                 tmcn::toUTF8('\u82F1\u6587\u6458\u8981')
                         )
                     }
-                    if (nchar(abs_cn)<10) cat(i,":",(i-1)*10+j,tmcn::toUTF8('\u6CA1\u6709\u6458\u8981'),
-                                              abs_url.j,'\n')
-                    if (nchar(abs_cn)>=10) cat(i,":",(i-1)*10+j,'\n')
+                    if (nchar(abs_cn)>=10) cat(tmcn::toUTF8(' \u627E\u52301\u4E2A'))
                 }
                 # cbind 2 parts
-                if (nrow(df_theme)==0) df.i = df_abstract
-                if (nrow(df_theme)!=0) df.i = cbind(df_theme,df_abstract)
-                df = rbind(df,df.i)
+                df.i = cbind(df_theme,df_abstract)
+                df = plyr::rbind.fill(df,df.i)
+            }else{
+                # no abstract
+                df.i = df_theme
+                df = plyr::rbind.fill(df,df.i)
             }
+        cat('\n')
         }
-        df=plyr::rbind.fill(df,df_theme_nokey)
         return(df)
     }
 }
